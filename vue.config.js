78bc4da02e1d2166578d719ec91bf2cfdda5bcd7
yennanliu <f339339@gmail.com>
2023-11-28 17:54:57 +0800

    rollback vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-//const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: 'vue-admin-system'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
