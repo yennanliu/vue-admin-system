05fc5cabd82a2a6c2c70f46d34201dc57c24cbb0
yennanliu <f339339@gmail.com>
2023-12-01 18:16:55 +0800

    update

--- src/views/Hotel/Hotel.vue
+++ src/views/Hotel/Hotel.vue
@@ -2,12 +2,30 @@
   <div class="about">
     <h1>This is HOTEL page !!!</h1>
     <button @click="increment">count is: {{ count }}</button>
+
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.text }}</li>
+    </ul>
+
   </div>
 </template>
   
 
-<script setup>
+<script>
 import { ref } from "vue";
+
+export default {
+  data() {
+    // TODO : read from csv
+    return {
+      items: [
+        { id: 1, text: "item 1" },
+        { id: 2, text: "item 2" },
+        { id: 3, text: "item 3" },
+      ],
+    };
+  },
+};
 
 const count = ref(0);
 
